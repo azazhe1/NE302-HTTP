d52381bf09a4b37f815768e3a04f291f833fb8bb
Cochonou26 <106807194+Cochonou26@users.noreply.github.com>
2023-02-28 19:39:26 +0100

    Add files via upload

---
+++ simple_parseur/include/fonctions.h
@@ -0,0 +1,12 @@
+#ifndef _FONCTIONS_
+#define _FONCTIONS_
+
+int Digit(char* message);
+int Htab(char* message);
+int Lf(char* message);
+int Sp(char* message);
+int Nombre (char* message);
+int Ponct(char* message);
+int separateur(char* message);
+
+#endif
