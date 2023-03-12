bf7786d75b877924745de5563c3ab5f7e6a37eb5
azazhe1 <106813232+azazhe1@users.noreply.github.com>
2023-03-12 23:41:52 +0100

    Delete simple_parseur directory

--- simple_parseur/include/grammaire.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "arbre.h"
-
-int Test_message(char *message);
-int Test_debut(char *message,Noeud *noeud);
-int Alpha(char *message, Noeud* noeud, int Is_pere);
-int Mot(char *message, Noeud *noeud); 
